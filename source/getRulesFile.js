0065237ada5f73f9b54715aba346d12c35cc77f8
Mael <mael.thomas@data.gouv.fr>
2019-02-12 09:34:23 +0000

    Nettoyage

--- source/getRulesFile.js
+++ source/getRulesFile.js
@@ -3,35 +3,20 @@ import yaml from 'js-yaml'
 
 let { GITHUB_TOKEN } = process.env
 
-/*
-function arrayBufferToBase64(buffer) {
-  var binary = '';
-  var bytes = [].slice.call(new Uint8Array(buffer));
-
-  bytes.forEach((b) => binary += String.fromCharCode(b));
-
-  return window.btoa(binary);
-};
-*/
-
 exports.handler = async (event, context) => {
 	let baseUrl = 'https://api.github.com/repos/',
 		repo = event.queryStringParameters.repo,
-		url =
-			baseUrl +
-			repo +
-			'/contents/' +
-			(event.queryStringParameters.filePath || 'co2.yaml')
-	var headers = {
-		Authorization: `token ${GITHUB_TOKEN}`,
-		Accept: 'application/vnd.github.v3.raw'
-	}
-	var options = {
-		method: 'GET',
-		headers: headers,
-		mode: 'cors',
-		cache: 'default'
-	}
+		url = baseUrl + repo + '/contents/' + event.queryStringParameters.filePath,
+		headers = {
+			Authorization: `token ${GITHUB_TOKEN}`,
+			Accept: 'application/vnd.github.v3.raw'
+		},
+		options = {
+			method: 'GET',
+			headers: headers,
+			mode: 'cors',
+			cache: 'default'
+		}
 
 	return fetch(url, options)
 		.then(response => {
